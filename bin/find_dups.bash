0e83dd4add9cdda5457c1e97ba7aabb15195ee29
Apache Tomcat <tomcat@ip-10-130-110-253.ec2.internal>
2022-09-28 20:06:01 +0000

    update

--- bin/find_dups.bash
+++ bin/find_dups.bash
@@ -4,10 +4,20 @@ for file in `cat /tmp/vt1.list`
 do 
     eadid=`egrep "eadid" $file | sed -e 's/></>\n</g' | egrep eadid | sed -e 's/.*">//' -e 's/<.*//' -e 's/xx\(.*\)xx/\1/' `
     if [[ -e "vt/${eadid}.xml" ]]; then
-        # echo $file $eadid "vt/${eadid}.xml"
-        oaititle=`cat $file | tr -d '\n' | sed -e 's#^.*\?<titleproper>##' -e 's#</titleproper>.*$##' | sed -e 's/ [ ]*/ /g'`
-        vttitle=`cat vt/${eadid}.xml | tr -d '\n' | sed -e 's#^.*\?<titleproper>##' -e 's#</titleproper>.*$##' | sed -e 's/ [ ]*/ /g'`
-        echo "vt/${eadid}.xml#${oaititle}#${vttitle}"
+        echo $file $eadid "vt/${eadid}.xml"
+        oaititle=`cat $file | tr -d '\n' | sed -e 's#^.*\?<titleproper>##' -e 's#</titleproper>.*$##' -e 's/<date.*//' | sed -e 's/ [ ]*/ /g'`
+        vttitle=`cat vt/${eadid}.xml | tr -d '\n' | sed -e 's#^.*\?<titleproper>##' -e 's#</titleproper>.*$##' -e 's/<date.*//' | sed -e 's/ [ ]*/ /g'`
+        if [[ ${oaititle} == ${vttitle} ]] ; then
+            echo "Match-y match"
+            cmp -s "vt/${eadid}.xml" "../vivaxtf/data/vt/${eadid}.xml" 
+            result=$?
+            if [[ "$result" == "0" ]] ; then 
+                mv "vt/${eadid}.xml" ../deleted/vt
+                #rm "../vivaxtf/vt/${eadid}.xml" 
+            fi
+        else    
+            echo "vt/${eadid}.xml#${oaititle}#${vttitle}"
+        fi
 #    else     
 #        if [[ "${eadid}" == "" ]] ; then 
 #            echo $file "no_id" "no_match" 
