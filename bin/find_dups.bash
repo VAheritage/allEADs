69c531d02183cd2bf36bfc1a9162ffea32758dec
Apache Tomcat <tomcat@ip-10-130-110-253.ec2.internal>
2022-09-28 22:06:01 +0000

    update

--- bin/find_dups.bash
+++ bin/find_dups.bash
@@ -5,8 +5,8 @@ do
     eadid=`egrep "eadid" $file | sed -e 's/></>\n</g' | egrep eadid | sed -e 's/.*">//' -e 's/<.*//' -e 's/xx\(.*\)xx/\1/' `
     if [[ -e "vt/${eadid}.xml" ]]; then
         echo $file $eadid "vt/${eadid}.xml"
-        oaititle=`cat $file | tr -d '\n' | sed -e 's#^.*\?<titleproper>##' -e 's#</titleproper>.*$##' -e 's/<date.*//' | sed -e 's/ [ ]*/ /g'`
-        vttitle=`cat vt/${eadid}.xml | tr -d '\n' | sed -e 's#^.*\?<titleproper>##' -e 's#</titleproper>.*$##' -e 's/<date.*//' | sed -e 's/ [ ]*/ /g'`
+        oaititle=`cat $file | tr -d '\r\n' | sed -e 's#^.*\?<titleproper>##' -e 's#</titleproper>.*$##' -e 's/<date.*//' -e 's/<num.*//' -e 's/ [ ]*/ /g' -e 's/[;,. ]*$//' -e 's/A Guide to \(the \)\?//' | tr "[]()" "()()"`
+        vttitle=`cat vt/${eadid}.xml | tr -d '\r\n' | sed -e 's#^.*\?<titleproper>##' -e 's#</titleproper>.*$##' -e 's/<date.*//' -e 's/<num.*//' -e 's/ [ ]*/ /g' -e 's/[;,. ]*$//' -e 's/A Guide to \(the \)\?//' | tr "[]()" "()()"`
         if [[ ${oaititle} == ${vttitle} ]] ; then
             echo "Match-y match"
             cmp -s "vt/${eadid}.xml" "../vivaxtf/data/vt/${eadid}.xml" 
@@ -16,7 +16,8 @@ do
                 #rm "../vivaxtf/vt/${eadid}.xml" 
             fi
         else    
-            echo "vt/${eadid}.xml#${oaititle}#${vttitle}"
+            diff=`diffcolor <(echo "${oaititle}) <(echo "${vttitle}")`
+            echo "vt/${eadid}.xml" $diff
         fi
 #    else     
 #        if [[ "${eadid}" == "" ]] ; then 
