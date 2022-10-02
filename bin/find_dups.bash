e4a42de1b8c0ada334482b3c6befd3cfbf671d63
Apache Tomcat <tomcat@ip-10-130-110-253.ec2.internal>
2022-10-02 14:06:02 +0000

    update

--- bin/find_dups.bash
+++ bin/find_dups.bash
@@ -13,8 +13,10 @@ do
     eadid=`egrep "eadid" $file | sed -e 's/></>\n</g' | egrep eadid | sed -e 's/.*">//' -e 's/<.*//' -e 's/xx\(.*\)xx/\1/' `
     if [[ -e "vt/${eadid}.xml" ]]; then
         echo $file $eadid "vt/${eadid}.xml"
-        oaititle=`cat $file | tr -d '\r\n' | sed -e 's#^.*\?<titleproper>##' -e 's#</titleproper>.*$##' -e 's/<date.*//' -e 's/<num.*//' -e 's/ [ ]*/ /g' -e 's/[;,. ]*$//' -e 's/A Guide to \(the \)\?//' | tr "[]()" "()()"`
-        vttitle=`cat vt/${eadid}.xml | tr -d '\r\n' | sed -e 's#^.*\?<titleproper>##' -e 's#</titleproper>.*$##' -e 's/<date.*//' -e 's/<num.*//' -e 's/ [ ]*/ /g' -e 's/[;,. ]*$//' -e 's/A Guide to \(the \)\?//' | tr "[]()" "()()"`
+        oaititle=`cat $file | tr -d '\r\n' | sed -e 's#^.*\?<titleproper>##' -e 's#</titleproper>.*$##' -e 's/<date.*//' -e 's/<num.*//' -e 's/ [ ]*/ /g' -e 's/[;":),. ]*$//' -e 's/A Guide to \(the \)\?//' | tr "[]()" "()()"`
+        vttitle=`cat vt/${eadid}.xml | tr -d '\r\n' | sed -e 's#^.*\?<titleproper>##' -e 's#</titleproper>.*$##' -e 's/<date.*//' -e 's/<num.*//' -e 's/ [ ]*/ /g' -e 's/[;,":). ]*$//' -e 's/A Guide to \(the \)\?//' | tr "[]()" "()()"`
+        oaititlestrip=`echo $oaititle | sed -e 's/[., ")(:;*]//g' -e "s/'//g"`
+        vttitlestrip=`echo $vttitle | sed -e 's/[., ")(:;*]//g' -e "s/'//g"`
         if [[ ${oaititle} == ${vttitle} ]] ; then
             echo "Match-y match"
             cmp -s "vt/${eadid}.xml" "../vivaxtf/data/vt/${eadid}.xml" 
@@ -28,6 +30,28 @@ do
             fi
         elif [[ "${oaititle}" == *"${vttitle}"* ]] ; then 
             echo "near Match-y match"
+            cmp -s "vt/${eadid}.xml" "../vivaxtf/data/vt/${eadid}.xml"
+            result=$?
+            if [[ "$result" == "0" ]] ; then
+                if [[ "$test" == 0 ]] ; then
+                    mv "vt/${eadid}.xml" ../deleted/vt
+                else
+                    echo mv "vt/${eadid}.xml" ../deleted/vt
+                fi
+            fi
+        elif [[ "${vttitle}" == *"${oaititle}"* ]] ; then 
+            echo "near Match-y match-y"
+            cmp -s "vt/${eadid}.xml" "../vivaxtf/data/vt/${eadid}.xml"
+            result=$?
+            if [[ "$result" == "0" ]] ; then
+                if [[ "$test" == 0 ]] ; then
+                    mv "vt/${eadid}.xml" ../deleted/vt
+                else
+                    echo mv "vt/${eadid}.xml" ../deleted/vt
+                fi
+            fi
+        elif [[ "${vttitlestrip}" == "${oaititlestrip}" ]] ; then 
+            echo "near Match-y match-y strip"
             cmp -s "vt/${eadid}.xml" "../vivaxtf/data/vt/${eadid}.xml"
             result=$?
             if [[ "$result" == "0" ]] ; then
